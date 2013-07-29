d95265f2490b23a8ccac52874636c1ecbba95a12
damirkusar <damir@kusar.ch>
2013-07-29 19:21:59 +0200

    Chapter 9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,31 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  attr_accessor :currentTime
-  before_filter :setupTime
+  attr_accessor :visitor
+  before_filter :current_visits
 
-  def setupTime
-    @currentTime = Time.now
+  private
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+
+  private
+  def current_visits
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+    @visitor = session[:counter]
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :image_url, :price, :title
+    attr_accessible :description, :image_url, :price, :title
   validates :description, :image_url, :price, :title, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true, length: {
@@ -10,4 +10,16 @@ class Product < ActiveRecord::Base
 	  with: 		%r{\.(gif|jpg|png|)$}i,
 	  message:	'must be a URL for GIF, JPG, or PNG image.'
   }
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
 end
