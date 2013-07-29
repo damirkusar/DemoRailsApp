e9a9a05f177f4360e279548ad08a1c9b0ffb6617
damirkusar <damir@kusar.ch>
2013-07-29 12:15:07 +0200

    chapter 8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  attr_accessor :currentTime
+  before_filter :setupTime
+
+  def setupTime
+    @currentTime = Time.now
+  end
 end
